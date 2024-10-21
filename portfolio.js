b2cdbe8c43d765f715f231d86c15f8991914235c
Mariaa888 <142675401+Mariaa888@users.noreply.github.com>
2024-10-21 16:14:37 +0300

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,21 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    let bar = document.querySelector(".navbar");
-    function show() {
-        bar.style.display = "flex";
-    }
-    function closebar() {
-        bar.style.display = 'none';
-    }
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
+let bar = document.querySelector(".navbar");
+function show() {
+    bar.style.display = "flex";
+}
+function closebar() {
+    bar.style.display = 'none';
+}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            }); 
-            if (window.innerWidth <= 720) {
-                closebar();
-            }
-        });
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        }); 
+        if (window.innerWidth < 720) {
+            closebar();
+        }
     });
 });
